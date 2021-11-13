9063e800c367496b66549f6b33fd33afb5096113
Gunmo Lee <77503982+rjsah5676@users.noreply.github.com>
2021-11-13 21:02:49 +0900

    네비게이션 바 수정 60%

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import './css/nav.css';
+import $ from "jquery";
 
 class Nav extends Component {
     render() {
@@ -23,8 +24,11 @@ class Nav extends Component {
         const buttonStyle = {
 
         };
-        
+        const subStyle = {
+            listStyleType: 'none'
+        };
         return(
+            /*
             <ul className="nav-bar">
                 <li onClick={goToInfo}>
                     <Navbar>
@@ -40,6 +44,11 @@ class Nav extends Component {
                     <Navbar style={buttonStyle}>
                         <NavLink className="nav-link" to="/melongame">멜론 게임</NavLink>
                     </Navbar>
+                    <div className="sub-menu">
+                        <Navbar style={buttonStyle}>
+                            <NavLink className="sub-link" to="/melongame">멜론 게임</NavLink>
+                        </Navbar>
+                    </div>
                 </li>
                 <li onClick={goTobb}>
                     <Navbar style={buttonStyle}>
@@ -52,6 +61,21 @@ class Nav extends Component {
                     </Navbar>
                 </li>
             </ul>
+            */
+           <ul id="test-nav">
+               <li><a href="#/info">소개</a></li>
+               <li><a href="#/algorithm">알고리즘 공부</a></li>
+               <li>
+                   <a className="test-menu" href="#/melongame">게임</a>
+                    <ul className="test-sub">
+                        <li><a href="#/melongame">멜론 게임</a></li>
+                        <li><a href="#">테슷트</a></li>
+                    </ul>
+               </li>
+               <li><a href="#/info">생각중</a></li>
+               <li><a href="#/algorithm">사진</a></li>
+               <div id="tesst"></div>
+           </ul>
         );
     };
 }
