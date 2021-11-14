2ca283f4a649d8cbe6a46c7769774f5f2f5dfdf1
Gunmo Lee <77503982+rjsah5676@users.noreply.github.com>
2021-11-15 02:10:29 +0900

    서버 클라이언트 연동

---
+++ servers/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+const io = socketio(server)
+
+//--for test
+const bodyParser = require('body-parser');
+app.use('/api', (req, res)=> res.json({username:'bryan'}));
+//--
+
+server.listen(PORT,()=>console.log(`서버가 ${PORT} 에서 시작되었어요`))
