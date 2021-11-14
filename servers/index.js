d9ea92e5fb94a7d4e4dc6e7a8e6bd768d269d805
Gunmo Lee <77503982+rjsah5676@users.noreply.github.com>
2021-11-15 03:25:36 +0900

    롤백

--- servers/index.js
+++ servers/index.js
@@ -1,7 +1,9 @@
+const functions = require('firebase-functions');
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000
 
@@ -9,10 +11,13 @@ const app = express();
 const server = http.createServer(app);
 app.use(cors());
 const io = socketio(server)
-
 //--for test
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../build')));
 app.use('/api', (req, res)=> res.json({username:'bryan'}));
 //--
 
-server.listen(PORT,()=>console.log(`서버가 ${PORT} 에서 시작되었어요`))
+server.listen(PORT,()=>console.log(`서버가 ${PORT} 에서 시작되었어요`,__dirname))
+
+exports.api = functions.https.onRequest(app);
--- src/Page/AlgoPage.jsx
+++ src/Page/AlgoPage.jsx
@@ -2,20 +2,11 @@ import React, { Component} from "react";
 import FadeIn from "../effect/FadeIn";
 
 class AlgoPage extends Component {
-  state={
-    username:""
-  }
-  componentDidMount() {
-    fetch('http://localhost:5000/api')
-        .then(res=>res.json())
-        .then(data=>this.setState({username:data.username}));
-  }
   render (){
     return (
       <FadeIn>
         <div>
-          알고페이지
-          {this.state.username}
+          테스트중uuu
         </div>
       </FadeIn>
     );
