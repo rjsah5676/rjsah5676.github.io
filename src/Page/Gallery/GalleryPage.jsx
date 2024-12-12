edcc2fc002a66199eb68244c36d15138ba84bf8b
unknown <rjsah5676@gmail.com>
2024-12-12 10:21:24 +0900

    gallery

--- src/Page/Gallery/GalleryPage.jsx
+++ src/Page/Gallery/GalleryPage.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Faded from "../../effect/Faded";
+import '../../css/gallery.css';
+import gallery_1 from '../../img/Gallery/gallery_1.png';
+import gallery_2 from '../../img/Gallery/gallery_2.png';
+import gallery_3 from '../../img/Gallery/gallery_3.png';
+import gallery_4 from '../../img/Gallery/gallery_4.png';
+import gallery_5 from '../../img/Gallery/gallery_5.png';
+import gallery_6 from '../../img/Gallery/gallery_6.png';
+import gallery_7 from '../../img/Gallery/gallery_7.png';
+import gallery_8 from '../../img/Gallery/gallery_8.png';
 
 function GalleryPage() {
     return (
       <Faded>
-        <div>
-          사진 추가 예정
+        <div className='gallery-container'>
+          <div className='gallery-box'>
+              <div className='gallery-item' style={{backgroundImage:`url(${gallery_1})`}}/>
+              <div className='gallery-item' style={{backgroundImage:`url(${gallery_2})`}}/>
+              <div className='gallery-item' style={{backgroundImage:`url(${gallery_3})`}}/>
+              <div className='gallery-item' style={{backgroundImage:`url(${gallery_4})`}}/>
+              <div className='gallery-item' style={{backgroundImage:`url(${gallery_5})`}}/>
+              <div className='gallery-item' style={{backgroundImage:`url(${gallery_6})`}}/>
+              <div className='gallery-item' style={{backgroundImage:`url(${gallery_7})`}}/>
+              <div className='gallery-item' style={{backgroundImage:`url(${gallery_8})`}}/>
+          </div>
         </div>
       </Faded>
     );
